4772f37f0cc2e8fc0805365d42e13aad9c89885b
byant2 <brianyant@gmail.com>
2025-07-01 19:52:37 -0400

    Bank updates

--- bank.c
+++ bank.c
@@ -1,13 +1,43 @@
 #include <stdio.h>
+#include <string.h>
+
+typedef struct {
+        // Define your key type (e.g., char*, int)
+        // Define your value type (e.g., char*, int)
+        char name[30];
+        int balance;
+    } Account;
 
 int main()
 {
+    char name[30];
     int num;
+    Account account[3];
+    int i = 0;
     
-    printf("Enter a number");
-    scanf("%d", &num);
+    while (i < 3){
+        printf("Enter a name\n");
+        scanf("%s", name);
 
-    printf("The User Enetered Integer = %d", num); 
-    
+        printf("The User Entered Name = %s\n", name); 
+        printf("Enter an amount to deposit\n"); 
+        scanf("%d", &num);
+        
+        strncpy(account[i].name, name, sizeof(account[i].name) - 1);
+        account[i].balance = num;
+
+        printf("Name = %s\n", account[i].name); 
+        printf("Deposit = %d\n", account[i].balance);
+        i++;
+    }
+
+    int j;
+    for (j = 0; j < 3; j++){
+        printf("Name = %s ", account[j].name); 
+        printf("Deposit = %d\n", account[j].balance);
+    }
+ 
+
     return 0;
 }
+
